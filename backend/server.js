e02beb438d4bdf53342d16da2a4ceab6fb0e2632
Mohamed Mydeen Shahabudeen M <156303564+mohamed-mydeen@users.noreply.github.com>
2026-02-06 23:51:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -106,8 +106,13 @@ app.delete("/games/:id", (req, res) => {
 });
 
 
+const PORT = process.env.PORT || 5000;
 
-  
-  
-
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
+
+  
+
+
+
